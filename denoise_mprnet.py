118c8f6140bb336edcd03849c779a7daa10f6b34
Kevin Selva Prasanna Valanarasu <kvalanar@purdue.edu>
2022-04-26 15:46:29 -0400

    Added denoise_mprnet function to denoise modified demo.py to run on CPU

--- denoise_mprnet.py
+++ denoise_mprnet.py
@@ -11,13 +11,7 @@ from natsort import natsorted
 from glob import glob
 import cv2
 import argparse
-
-parser = argparse.ArgumentParser(description='Demo MPRNet')
-parser.add_argument('--input_dir', default='./samples/input/', type=str, help='Input images')
-parser.add_argument('--result_dir', default='./samples/output/', type=str, help='Directory for results')
-parser.add_argument('--task', required=True, type=str, help='Task to run', choices=['Deblurring', 'Denoising', 'Deraining'])
-
-args = parser.parse_args()
+import numpy as np
 
 def save_img(filepath, img):
     cv2.imwrite(filepath,cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
@@ -34,34 +28,23 @@ def load_checkpoint(model, weights):
             new_state_dict[name] = v
         model.load_state_dict(new_state_dict)
 
-task    = args.task
-inp_dir = args.input_dir
-out_dir = args.result_dir
+task = 'Denoising'
 
-os.makedirs(out_dir, exist_ok=True)
+def denoise_mprnet(img):
 
-files = natsorted(glob(os.path.join(inp_dir, '*.jpg'))
-                + glob(os.path.join(inp_dir, '*.JPG'))
-                + glob(os.path.join(inp_dir, '*.png'))
-                + glob(os.path.join(inp_dir, '*.PNG')))
+    # Load corresponding model architecture and weights
+    load_file = run_path(os.path.join("MPRNet/", task, "MPRNet.py"))
+    model = load_file['MPRNet']()
+    #model.cuda()
 
-if len(files) == 0:
-    raise Exception(f"No files found at {inp_dir}")
+    weights = os.path.join("MPRNet/",task, "pretrained_models", "model_"+task.lower()+".pth")
+    load_checkpoint(model, weights)
+    model.eval()
 
-# Load corresponding model architecture and weights
-load_file = run_path(os.path.join(task, "MPRNet.py"))
-model = load_file['MPRNet']()
-#model.cuda()
-
-weights = os.path.join(task, "pretrained_models", "model_"+task.lower()+".pth")
-load_checkpoint(model, weights)
-model.eval()
-
-img_multiple_of = 8
-
-for file_ in files:
-    img = Image.open(file_).convert('RGB')
-    input_ = TF.to_tensor(img).unsqueeze(0) #.cuda()
+    img_multiple_of = 8
+    #img = Image.fromarray(np.uint8(img*255))
+    #input_ = TF.to_tensor(img).unsqueeze(0) #.cuda()
+    input_ = img
 
     # Pad the input if not_multiple_of 8
     h,w = input_.shape[2], input_.shape[3]
@@ -81,7 +64,4 @@ for file_ in files:
     restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
     restored = img_as_ubyte(restored[0])
 
-    f = os.path.splitext(os.path.split(file_)[-1])[0]
-    save_img((os.path.join(out_dir, f+'.png')), restored)
-
-print(f"Files saved at {out_dir}")
+    return restored
